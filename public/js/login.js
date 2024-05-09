57a61b874773b5381e52fa2fdf4b7de8239cd3e1
Seth Philpot <seth_philpot@yahoo.com>
2024-05-09 15:17:54 -0400

    login view and login js

---
+++ public/js/login.js
@@ -0,0 +1,18 @@
+const loginForm = document.querySelector(".loginForm");
+
+const handleLogin = async(event) => {
+  event.preventDefault()
+  const email = document.querySelector("#emailinput").value
+  const password = document.querySelector("#passwordinput").value
+  if (!email && !password) return;
+  const response = await fetch("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: { "Content-Type": "application/json" }
+  })
+  if (response.ok) {
+    document.location.replace(document.referrer)
+  }
+};
+
+loginForm.addEventListener("submit", handleLogin);
