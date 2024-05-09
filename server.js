282877bb05d9d13608f4576561550afe2943e9ff
Larry Wisniewski <larryw48390@gmail.com>
2024-05-08 23:47:52 -0400

    prettier and eslint configuration

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const path = require('path');
 const express = require('express');
 const expressSession = require('express-session');
 const expressHandleBars = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(expressSession.Store);
 const handleBarHelpers = require('./libs/handlebars_helpers/helpers');
 const sequelize = require('./config/config');
-const SequelizeStore = require('connect-session-sequelize')(expressSession.Store);
+
 const PORT = process.env.PORT || 3001;
 
 const routes = require('./controllers');
@@ -12,19 +13,19 @@ const routes = require('./controllers');
 const app = express();
 
 const session = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {
-        maxAge: 1000 * 60 * 60,
-        httpOnlly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'lax',
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    httpOnlly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: 'lax',
 
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-    }),
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
 };
 
 app.use(expressSession(session));
@@ -38,9 +39,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
-
