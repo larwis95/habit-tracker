f2982a4585735a5ee4839ac264e1c8b3c249ac29
FonKnp <fonnfonk@gmail.com>
2024-05-10 14:08:22 +0200

    close get dashboard function

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,26 @@
-const router = require('express').Router();
-const withAuth = require('../libs/middleware/auth.js');
-const { User, Habit, Pet, State } = require('../models');
+const router = require("express").Router();
+const withAuth = require("../libs/middleware/auth.js");
+const { User, Habit, Pet, State } = require("../models");
 
-router.get('/', async (req, res) => {
-  res.render('homepage', {
+router.get("/", async (req, res) => {
+  res.render("homepage", {
     logged_in: req.session.logged_in,
   });
 });
 
-router.get('/dashboard', withAuth, async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {});
 
-router.get('/login', async (req, res) => {
-  res.render('login');
+router.get("/login", async (req, res) => {
+  res.render("login");
 });
 
-router.get('/signup', async (req, res) => {
-  res.render('signup');
+router.get("/signup", async (req, res) => {
+  res.render("signup");
 });
 
 router.get(/logout/, async (req, res) => {
   req.session.destroy(() => {
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
