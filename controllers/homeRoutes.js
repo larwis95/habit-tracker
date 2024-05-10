b1a192aff8fec83e8f30bf16be77e7cce3b3010a
Larry Wisniewski <larryw48390@gmail.com>
2024-05-10 00:53:06 -0400

    fixed some file structure, added dashboard route, added wuth middleware

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
+const withAuth = require('../libs/middleware/auth.js');
+const { User, Habit, Pet, State } = require('../models');
 
 router.get('/', async (req, res) => {
   res.render('homepage', {
     logged_in: req.session.logged_in,
   });
 });
+
+router.get('/dashboard', withAuth, async (req, res) => {
 
 router.get('/login', async (req, res) => {
   res.render('login');
