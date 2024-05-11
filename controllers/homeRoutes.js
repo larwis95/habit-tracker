ecaf586a2fe996d38dfb7f13f77de37c63bb5ddb
Larry Wisniewski <larryw48390@gmail.com>
2024-05-10 23:58:05 -0400

    fixed homeroutes had an older version in my local branch

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,42 @@
-const router = require("express").Router();
-const withAuth = require("../libs/middleware/auth.js");
-const { User, Habit, Pet, State } = require("../models");
+const router = require('express').Router();
+const withAuth = require('../libs/middleware/auth');
+const { User, Habit, Pet, State } = require('../models');
 
-router.get("/", async (req, res) => {
-  res.render("homepage", {
+router.get('/', async (req, res) => {
+  res.render('homepage', {
     logged_in: req.session.logged_in,
   });
 });
 
-router.get("/dashboard", withAuth, async (req, res) => {});
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Habit }, { model: Pet }, { model: State }],
+    });
 
-router.get("/login", async (req, res) => {
-  res.render("login");
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+router.get('/login', async (req, res) => {
+  res.render('login');
 });
 
-router.get("/signup", async (req, res) => {
-  res.render("signup");
+router.get('/signup', async (req, res) => {
+  res.render('signup');
 });
 
 router.get(/logout/, async (req, res) => {
   req.session.destroy(() => {
-    res.redirect("/");
+    res.redirect('/');
   });
 });
 
