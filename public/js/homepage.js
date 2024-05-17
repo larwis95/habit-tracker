4bf327349ede5233eb20a90980142acefa4cd3ac
Larry Wisniewski <larryw48390@gmail.com>
2024-05-17 03:47:53 -0400

    styling changes animation js cleanup

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,39 +1,21 @@
 /* eslint-disable no-param-reassign */
 const petContainter = document.querySelector('.homePagePets');
-const pets = document.querySelectorAll('.petImage');
-let lastScroll = 0;
-let right = 0;
 
-const scrollHandler = () => {
-  if (window.scrollY < lastScroll) {
-    if (right > 100) {
-      right = 100;
-    }
-   petContainter.style.setProperty('--right', `${right += 10}%`);
-   pets.forEach(pet => {
-    pet.style.objectPosition = `${right + 40 + 100}% 50%`;
-  });
-  }
-  else {
-    if (right < 0) {
-      right = 0;
-    }
-    petContainter.style.setProperty('--right', `${right -= 10}%`);
-    pets.forEach(pet => {
-      pet.style.objectPosition = `${right - 40 + 100}% 50%`;
-    });
-  }
-};
+let lastScroll = 0;
 
 const scrollEndHandler = () => {
   lastScroll = window.scrollY;
+  if (lastScroll > 400) {
   petContainter.style.setProperty('--right', `0%`);
+  }
+  else {
+    petContainter.style.setProperty('--right', `100%`);
+  }
 };
 
 const handleDOMContentLoaded = async () => {
   petContainter.style.setProperty('--right', `100%`);
 }
 
-document.addEventListener('scroll', scrollHandler);
 document.addEventListener('scrollend', scrollEndHandler);
 document.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
